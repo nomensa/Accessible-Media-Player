91c43dfc3abc7ed64e8960e3446f3b14030f496e
tombye <tom.byers@digital.cabinet-office.gov.uk>
2012-12-11 16:04:31 +0000

    Ensure Youtube Player is invoked correctly
    Make sure
    
    - the constructor isn't called with non-Youtube video URLs
    - it is called with the correct parameters.

--- test/jquery_player_test.js
+++ test/jquery_player_test.js
@@ -16,10 +16,67 @@ describe("jquery.player tests (integration)", function () {
     expect(document.querySelectorAll("#wrapper").length).toBe(1);
   });
 
+  it("should not call the YoutubePlayer constructor when using Vimeo", function () {
+    var videoLink, youtubeId;
+
+    videoLink = document.createElement("a");
+    videoLink.href = "http://vimeo.com/36579366";
+    youtubeId = videoLink.href.split("=")[1];
+
+    wrapper.appendChild(videoLink);
+
+    spyOn(window, "YoutubePlayer");
+
+    holder = $("<span></span>");
+    holder.append(videoLink);
+    $("#wrapper").append(holder);
+
+    // Initialise the player.
+    holder.player({
+      id: 'youtube1',
+      media: youtubeId,
+      url: videoLink.href
+    });
+
+    expect(window.YoutubePlayer).not.toHaveBeenCalled(); 
+
+    holder.remove();
+    wrapper.appendChild(videoLink);
+  });
+
   describe("Youtube videos", function () {
     var videoLink, youtubeId;
 
     beforeEach(function () {
+      defaultConfig = {
+          id: 'media_player',
+          url: 'http://www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=',
+          media: '8LiQ-bLJaM4',
+          repeat: false,
+          captions: null,
+          captionsOn: true,
+          flashWidth: '100%',
+          flashHeight: '300px',
+          playerStyles: {
+              'height': '100%',
+              'width': '100%'
+          },
+          sliderTimeout: 350,
+          flashContainer: 'span',
+          playerContainer: 'span',
+          image: '',
+          playerSkip: 10,
+          volumeStep: 10,
+          buttons: {
+              forward: true,
+              rewind: true,
+              toggle: true
+          },
+          logoURL: 'http://www.nomensa.com?ref=logo',
+          useHtml5: true,
+          swfCallback: null
+      };
+
       videoLink = document.createElement("a");
       videoLink.href = "http://www.youtube.com/watch?v=qySFp3qnVmM";
 
@@ -38,10 +95,11 @@ describe("jquery.player tests (integration)", function () {
       expect(document.querySelectorAll("#wrapper a[href*='http://www.youtube.com/watch']").length).toBe(1);
     });
 
-    describe("create a jquery player", function () {
+    describe("Create a Youtube Player", function () {
       var holder;
 
       beforeEach(function () {
+        expectedConfig = defaultConfig;
         holder = $("<span></span>");
         holder.append(videoLink);
         $("#wrapper").append(holder);
@@ -51,6 +109,9 @@ describe("jquery.player tests (integration)", function () {
           id: 'youtube1',
           media: youtubeId
         });
+
+        expectedConfig.id = 'youtube1';
+        expectedConfig.media = youtubeId;
       });
 
       afterEach(function () {
@@ -66,12 +127,18 @@ describe("jquery.player tests (integration)", function () {
         expect(window.YoutubePlayer).toHaveBeenCalled();
       });
 
-      it("should call the Youtube constructor with a single argument", function () {
-        expect(window.YoutubePlayer.calls[0].args[0]).toBeDefined();
-      });
+      describe("call the YoutubePlayer constructor", function () {
+        it("should recieve a single argument", function () {
+          expect(window.YoutubePlayer.calls[0].args[0]).toBeDefined();
+        });
 
-      it("should call the Youtube constructor with an object", function () {
-        expect(typeof window.YoutubePlayer.calls[0].args[0] === "object").toBe(true);
+        it("should recieve an object", function () {
+          expect(typeof window.YoutubePlayer.calls[0].args[0] === "object").toBe(true);
+        });
+
+        it("should recieve the default config", function () {
+          expect(window.YoutubePlayer.calls[0].args[0]).toEqual(expectedConfig);
+        });
       });
     });
   });
