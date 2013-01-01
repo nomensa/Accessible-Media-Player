b4be06e0c46aaca6c6b7d9ecdf2c683c9961704e
tombye <tom.byers@digital.cabinet-office.gov.uk>
2013-01-01 22:35:18 +0000

    Corrections and amends to tests
    Remove tests dependant on window.postMessage & namespace the
    PlayerManager tests.

--- test/player_manager_spec.js
+++ test/player_manager_spec.js
@@ -1,12 +1,13 @@
-describe("PlayerManager", function () {
+window.nomensaPlayer = window.nomensaPlayer || {};
+describe("window.nomensaPlayer.PlayerManager", function () {
   var manager;
 
   beforeEach(function () {
-    manager = new PlayerManager();
+    manager = new window.nomensaPlayer.PlayerManager();
   });
 
-  it("should have PlayerManager defined", function () {
-    expect(PlayerManager).toBeDefined();
+  it("should have window.nomensaPlayer.PlayerManager defined", function () {
+    expect(window.nomensaPlayer.PlayerManager).toBeDefined();
   });
 
   it("should not have an unknown player", function () {
@@ -22,10 +23,29 @@ describe("PlayerManager", function () {
       };
     });
 
+    afterEach(function () {
+      examplePlayer = null;
+    });
+
     it("should be able to set a player", function () {
       expect(manager.getPlayer(123)).toEqual(null);
       manager.addPlayer(examplePlayer);
       expect(manager.getPlayer(123)).toEqual(examplePlayer);
+    });
+
+    it("should be able to map a function", function () {
+      var total = 0,
+          examplePlayer2 = {
+            config: {
+              id: 1
+            }
+          };
+      manager.addPlayer(examplePlayer);
+      manager.addPlayer(examplePlayer2);
+      manager.map(function (player) {
+        total += player.config.id;
+      })
+      expect(total).toEqual(124);
     });
   });
 });
