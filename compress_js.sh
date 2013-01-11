3798ef6404232a99e4a3a16880f7af0c275ae46a
tombye <tom.byers@digital.cabinet-office.gov.uk>
2013-01-11 15:52:26 +0000

    Add header through build & give build config
    Build script will now build only for the players specified

--- compress_js.sh
+++ compress_js.sh
@@ -1,29 +1,69 @@
 #! /bin/bash
+#
+# Compress the required media player JavaScripts into one file
+#
+
+usage()
+{
+cat << EOF
+Usage: $0 [players...]
+
+Compress the required media player JavaScripts into one file
+
+EXAMPLES:
+  compress_js.sh vimeo
+  compress_js.sh vimeo jwplayer
+
+EOF
+}
 
 ANSI_GREEN="\033[32m"
 ANSI_YELLOW="\033[33m"
 ANSI_RESET="\033[0m"
 
-dir=`pwd`
-plugin="${dir}/core/javascript/jquery.player.js"
-decorator="${dir}/core/javascript/mediaplayer_decorator.js"
-swfobject="${dir}/core/javascript/swfobject/swfobject.js"
-players[0]="${dir}/custom/javascript/config/jwplayer-5/jw-player.config.js"
-players[1]="${dir}/custom/javascript/config/vimeo/vimeo.config.js"
-players[2]="${dir}/core/javascript/youtube_player.js"
+header="./build/header.txt"
+plugin="./core/javascript/jquery.player.js"
+decorator="./core/javascript/mediaplayer_decorator.js"
+swfobject="./core/javascript/swfobject/swfobject.js"
+jwplayer[0]="./custom/javascript/config/jwplayer-5/core/jwplayer.js"
+jwplayer[1]="./custom/javascript/config/jwplayer-5/jw-player.config.js"
+vimeo="./custom/javascript/config/vimeo/vimeo.config.js"
+players[0]="./core/javascript/youtube_player.js"
 
-output="${dir}/core/javascript/jquery.player.min.js"
-temp=$dir/build/temp.js
+output="./core/javascript/jquery.player.min.js"
+compressed="./build/compressed.js"
+combined="./build/combined.js"
 
 function on_exit()
 {
-  rm -f ${temp}
+  rm -f ${combined}
+  rm -f ${compressed}
 }
 
 trap on_exit INT TERM EXIT
 
+# Add any sent players
+while [ "$1" != "" ]; do
+  case $1 in
+    h )               
+      usage
+      exit 1
+      ;;
+    "jwplayer" )      
+      players[${#players[@]}]=${jwplayer[0]}
+      players[${#players[@]}]=${jwplayer[1]}
+      ;;
+    "vimeo" )         
+      players[${#players[@]}]=${vimeo}
+      ;;
+  esac
+  shift
+done
+
+echo ${players[@]}
+
 # combine all scripts
-cat $swfobject ${players[@]} ${decorator} ${plugin} > $temp
+cat $swfobject ${players[@]} ${decorator} ${plugin} > $combined
 
 echo -e "\nCompressing\n"
 echo -e "${ANSI_GREEN}${swfobject}"
@@ -37,6 +77,8 @@ echo -e "\ninto\n"
 echo -e "${ANSI_YELLOW}${output}${ANSI_RESET}"
 
 # compress
-java -jar ./build/yuicompressor-2.4.2.jar $temp --type js --charset utf-8 --preserve-semi -o ${output}
+java -jar "./build/yuicompressor-2.4.2.jar" $combined --type js --charset utf-8 --preserve-semi -o $compressed
+
+cat $header $compressed > $output
 
 echo -e "\nDone!"
